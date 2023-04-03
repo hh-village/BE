24e289a030ce487fb788c80d228b3b462bcf36e3
YINGMEI <j.yeongmee@gmail.com>
2023-04-03 22:52:32 +0900

    feat: 예약 취소 기능 구현 완료

--- src/main/java/com/sparta/village/global/exception/ErrorCode.java
+++ src/main/java/com/sparta/village/global/exception/ErrorCode.java
@@ -18,12 +18,12 @@ public enum ErrorCode {
     NOT_PROPER_PASSWORD(BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
     NOT_PROPER_INPUTFORM(BAD_REQUEST, "입력한 형식이 맞지 않습니다."),
     NOT_PROPER_URLFORM(BAD_REQUEST, "입력한 URL 형식이 맞지 않습니다."),
-    NOT_AUTHOR(BAD_REQUEST, "작성자만 삭제/수정할 수 있습니다."),
+    NOT_AUTHOR(BAD_REQUEST, "예약 취소 권한이 없습니다."),
     WRONG_ADMIN_TOKEN(BAD_REQUEST, "관리자 암호가 틀려 등록이 불가능합니다."),
 
     /* 404 NOT_FOUND : Resource 를 찾을 수 없음 */
     USER_NOT_FOUND(NOT_FOUND, "등록된 사용자가 없습니다"),
-    POST_NOT_FOUND(NOT_FOUND, "선택한 게시물을 찾을 수 없습니다."),
+    RESERVATION_NOT_FOUND(NOT_FOUND, "선택한 댓글에 해당되는 예약이 없습니다."),
     COMMENT_NOT_FOUND(NOT_FOUND, "선택한 댓글을 찾을 수 없습니다.");
 
 
