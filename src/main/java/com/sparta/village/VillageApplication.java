18867da1de972953ddc6469b9c3c7746a0e029af
YINGMEI <j.yeongmee@gmail.com>
2023-04-03 12:26:51 +0900

    feat: security 적용

--- src/main/java/com/sparta/village/VillageApplication.java
+++ src/main/java/com/sparta/village/VillageApplication.java
@@ -2,8 +2,10 @@ package com.sparta.village;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class VillageApplication {
 
     public static void main(String[] args) {
