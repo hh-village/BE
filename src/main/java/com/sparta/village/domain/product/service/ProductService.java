1a1312f311d839b34d4e56897e7b14b1e7eed208
ㅡApfelsaft-ASAP <myoungbeen@gmail.com>
2023-04-04 18:00:29 +0800

    feat : 제품등록 + 이미지 업로드 기능구현

--- src/main/java/com/sparta/village/domain/product/service/ProductService.java
+++ src/main/java/com/sparta/village/domain/product/service/ProductService.java
@@ -30,7 +30,6 @@ public class ProductService {
         // 이미지를 S3에 업로드하고 파일 URL 목록을 가져옴
         ResponseEntity<ResponseMessage> response = imageStorageService.storeFiles(productRequestDto.getImages());
         List<String> fileUrls = (List<String>) response.getBody().getData();
-        System.out.println(fileUrls.toString());
 
         // 새로운 Product 객체를 생성하고 저장합니다.
         Product newProduct = new Product(user, productRequestDto);
