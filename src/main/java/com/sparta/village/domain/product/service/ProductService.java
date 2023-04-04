8e5a29e547d4d6bc765d3933840acbf7605f018b
YINGMEI <j.yeongmee@gmail.com>
2023-04-04 18:21:16 +0900

    feat: 예약 상태변경하기 구현 완료

--- src/main/java/com/sparta/village/domain/product/service/ProductService.java
+++ src/main/java/com/sparta/village/domain/product/service/ProductService.java
@@ -4,10 +4,15 @@ import com.sparta.village.domain.product.dto.ProductRequestDto;
 import com.sparta.village.domain.product.entity.Product;
 import com.sparta.village.domain.product.repository.ProductRepository;
 import com.sparta.village.domain.user.entity.User;
+import com.sparta.village.global.exception.CustomException;
+import com.sparta.village.global.exception.ErrorCode;
 import com.sparta.village.global.exception.ResponseMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +21,20 @@ public class ProductService {
     public ResponseEntity<ResponseMessage> registProduct(User user, ProductRequestDto productRequestDto) {
         productRepository.saveAndFlush(new Product(user, productRequestDto));
         return ResponseMessage.SuccessResponse("성공적으로 제품 등록이 되었습니다.", "");
+    }
+
+    @Transactional
+    public void checkProductId(Long id) {
+        if (!productRepository.existsById(id)) {
+            throw new CustomException(ErrorCode.PRODUCT_NOT_FOUND);
+        }
+    }
+
+    @Transactional
+    public void checkProductOwner(Long productId, Long userId) {
+        if (!productRepository.existsByIdAndUserId(productId, userId)) {
+            throw new CustomException(ErrorCode.NOT_SELLER);
+        }
     }
 
 }
