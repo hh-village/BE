23fe56d9e933d1be095cf7a03ef9444a4583d6ad
YINGMEI <j.yeongmee@gmail.com>
2023-04-05 17:54:46 +0900

    feat: 예약리스트 반환하는 함수 추가

---
+++ src/main/java/com/sparta/village/domain/reservation/dto/ReservationResponseDto.java
@@ -0,0 +1,21 @@
+package com.sparta.village.domain.reservation.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class ReservationResponseDto {
+    private Long id;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    private String status;
+    private String nickname;
+}
