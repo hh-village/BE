2c38762e5cfe45881cc92d9fa7dd549bb1d73872
ㅡApfelsaft-ASAP <myoungbeen@gmail.com>
2023-04-11 03:01:36 +0800

    fix : 유저정보 확인 변경

--- src/main/java/com/sparta/village/domain/user/repository/UserRepository.java
+++ src/main/java/com/sparta/village/domain/user/repository/UserRepository.java
@@ -10,6 +10,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByKakaoId(Long kakaoId);
 
 
-    boolean existsByKakaoId(Long kakaoId);
+    Optional<User> findByNickname(String nickname);
 
 }
