5c5c1b30b08873c77572d042aa2dfbff7e9393ec
ㅡApfelsaft-ASAP <myoungbeen@gmail.com>
2023-04-15 21:32:02 +0800

    fix : 제품 삭제시 채팅 메세지, 채팅룸, 예약, 찜, 이미지 삭제 될수 있게 기능수정

--- src/main/java/com/sparta/village/domain/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/sparta/village/domain/chat/repository/ChatRoomRepository.java
@@ -4,6 +4,7 @@ import com.sparta.village.domain.chat.entity.ChatRoom;
 import com.sparta.village.domain.product.entity.Product;
 import com.sparta.village.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -17,4 +18,8 @@ public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
 
     @Query(value = "select r from ChatRoom r where r.user = :user or r.owner = :user")
     List<ChatRoom> findAllChatRoomByUser(@Param("user") User user);
+
+    @Modifying
+    @Query("DELETE FROM ChatRoom cr WHERE cr.product.id = :productId")
+    void deleteByProductId(@Param("productId") Long productId);
 }
--- src/main/java/com/sparta/village/domain/zzim/repository/ZzimRepository.java
+++ src/main/java/com/sparta/village/domain/zzim/repository/ZzimRepository.java
@@ -4,6 +4,9 @@ import com.sparta.village.domain.product.entity.Product;
 import com.sparta.village.domain.user.entity.User;
 import com.sparta.village.domain.zzim.entity.Zzim;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,5 +19,9 @@ public interface ZzimRepository extends JpaRepository<Zzim, Long> {
     List<Zzim> findByUser(User user);
 
     int countByUser(User user);
+
+    @Modifying
+    @Query("DELETE FROM Zzim z WHERE z.product.id = :productId")
+    void deleteByProductId(@Param("productId") Long Id);
 
 }
