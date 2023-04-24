4b599d9d3443b394a9354340a7e9fea0b59110b3
YINGMEI <j.yeongmee@gmail.com>
2023-04-25 00:34:52 +0900

    feat: 채팅방 구독자에게 채팅시간도 같이 응답 보냄

--- src/main/java/com/sparta/village/domain/chat/service/ChatService.java
+++ src/main/java/com/sparta/village/domain/chat/service/ChatService.java
@@ -1,9 +1,6 @@
 package com.sparta.village.domain.chat.service;
 
-import com.sparta.village.domain.chat.dto.ChatMessageDto;
-import com.sparta.village.domain.chat.dto.ChatMessageResponseDto;
-import com.sparta.village.domain.chat.dto.MessageListDto;
-import com.sparta.village.domain.chat.dto.RoomListDto;
+import com.sparta.village.domain.chat.dto.*;
 import com.sparta.village.domain.chat.entity.ChatMessage;
 import com.sparta.village.domain.chat.entity.ChatRoom;
 import com.sparta.village.domain.chat.repository.ChatMessageRepository;
@@ -21,7 +18,6 @@ import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
@@ -57,7 +53,7 @@ public class ChatService {
                 roomId = roomList.get(0);
             }
         }
-        ChatMessageResponseDto data = roomId == null ? null : findMessageHistoryByRoomId(roomId, user);
+        MyChatRoomResponseDto data = roomId == null ? null : findMessageHistoryByRoomId(roomId, user);
         return ResponseMessage.SuccessResponse("대화 불러오기 성공", data);
     }
 
@@ -65,15 +61,17 @@ public class ChatService {
     public void saveMessage(ChatMessageDto message) {
         User user = userService.getUserByNickname(message.getSender());
         ChatRoom room = chatRoomRepository.findByRoomId(message.getRoomId()).orElseThrow(() -> new CustomException(ErrorCode.CHATROOM_NOT_FOUND));
-        chatMessageRepository.saveAndFlush(new ChatMessage(user, message.getContent(), room));
-        template.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+        ChatMessage chatMessage = new ChatMessage(user, message.getContent(), room);
+        chatMessageRepository.saveAndFlush(chatMessage);
+        ChatMessageResponseDto responseDto = new ChatMessageResponseDto(chatMessage.getRoom().getRoomId(), chatMessage.getSender().getNickname(), chatMessage.getContent(), changeDateFormat(chatMessage.getCreatedAt()));
+        template.convertAndSend("/sub/chat/room/" + message.getRoomId(), responseDto);
     }
 
     private User getConversationPartner(ChatRoom chatRoom, User user) {
         return chatRoom.getUser().getId().equals(user.getId()) ? chatRoom.getOwner() : chatRoom.getUser();
     }
 
-    private ChatMessageResponseDto findMessageHistoryByRoomId(String roomId, User user) {
+    private MyChatRoomResponseDto findMessageHistoryByRoomId(String roomId, User user) {
         ChatRoom room = chatRoomRepository.findByRoomId(roomId).orElseThrow(() -> new CustomException(ErrorCode.CHATROOM_NOT_FOUND));
         List<MessageListDto> messageList = chatMessageRepository.findAllByRoom(room).stream()
                 .map(m -> new MessageListDto(m.getSender().getNickname(), m.getContent(), m.getRoom().getRoomId(), changeDateFormat(m.getCreatedAt()))).toList();
@@ -86,7 +84,7 @@ public class ChatService {
             String lastMessage = chatMessageList.size() > 0 ? chatMessageList.get(0).getContent() : null;
             roomList.add(new RoomListDto(chatRoom.getRoomId(), other.getNickname(), other.getProfile(), lastMessage, target));
         }
-        return new ChatMessageResponseDto(messageList, roomList);
+        return new MyChatRoomResponseDto(messageList, roomList);
     }
 
     private String changeDateFormat(String createdAt) {
