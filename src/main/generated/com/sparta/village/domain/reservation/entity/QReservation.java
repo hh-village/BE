cc5bbf953df5e71f2f725f1054211040be919df2
YINGMEI <j.yeongmee@gmail.com>
2023-04-25 17:38:36 +0900

    feat: querydsl Qclass 추가

---
+++ src/main/generated/com/sparta/village/domain/reservation/entity/QReservation.java
@@ -0,0 +1,68 @@
+package com.sparta.village.domain.reservation.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QReservation is a Querydsl query type for Reservation
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QReservation extends EntityPathBase<Reservation> {
+
+    private static final long serialVersionUID = -1446936059L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QReservation reservation = new QReservation("reservation");
+
+    public final QTimestamped _super = new QTimestamped(this);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    public final DatePath<java.time.LocalDate> endDate = createDate("endDate", java.time.LocalDate.class);
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> modifiedAt = _super.modifiedAt;
+
+    public final com.sparta.village.domain.product.entity.QProduct product;
+
+    public final DatePath<java.time.LocalDate> startDate = createDate("startDate", java.time.LocalDate.class);
+
+    public final StringPath status = createString("status");
+
+    public final com.sparta.village.domain.user.entity.QUser user;
+
+    public QReservation(String variable) {
+        this(Reservation.class, forVariable(variable), INITS);
+    }
+
+    public QReservation(Path<? extends Reservation> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QReservation(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QReservation(PathMetadata metadata, PathInits inits) {
+        this(Reservation.class, metadata, inits);
+    }
+
+    public QReservation(Class<? extends Reservation> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.product = inits.isInitialized("product") ? new com.sparta.village.domain.product.entity.QProduct(forProperty("product"), inits.get("product")) : null;
+        this.user = inits.isInitialized("user") ? new com.sparta.village.domain.user.entity.QUser(forProperty("user")) : null;
+    }
+
+}
+
---
+++ src/main/generated/com/sparta/village/domain/user/entity/QUser.java
@@ -0,0 +1,45 @@
+package com.sparta.village.domain.user.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QUser is a Querydsl query type for User
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QUser extends EntityPathBase<User> {
+
+    private static final long serialVersionUID = 425596789L;
+
+    public static final QUser user = new QUser("user");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final NumberPath<Long> kakaoId = createNumber("kakaoId", Long.class);
+
+    public final StringPath nickname = createString("nickname");
+
+    public final StringPath profile = createString("profile");
+
+    public final EnumPath<UserRoleEnum> role = createEnum("role", UserRoleEnum.class);
+
+    public QUser(String variable) {
+        super(User.class, forVariable(variable));
+    }
+
+    public QUser(Path<? extends User> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QUser(PathMetadata metadata) {
+        super(User.class, metadata);
+    }
+
+}
+
