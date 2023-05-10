6cb9739fe135f54ed29e39ab555136851512aaac
YINGMEI <j.yeongmee@gmail.com>
2023-05-11 01:52:45 +0900

    docs: cors 설정 추가

--- src/main/java/com/sparta/village/global/config/CorsConfig.java
+++ src/main/java/com/sparta/village/global/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:3000", "http://team3-village.s3-website.ap-northeast-2.amazonaws.com/", "https://dom4ncqkp60qx.cloudfront.net", "https://village99.store"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "https://village99.vercel.app", "https://dom4ncqkp60qx.cloudfront.net", "https://village99.store"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("*"));
